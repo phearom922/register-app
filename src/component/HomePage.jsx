d6ce552157ae9314a0f44fc2b140c2530e202b18
phearom922 <ronphearom2540@gmail.com>
2025-02-10 14:58:34 +0700

    Initial commit

---
+++ src/component/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+const HomePage = () => {
+  return (
+    <div className='grid grid-cols-6 gap-5 py-10'>
+      <img className='object-cover h-full object-center rounded-2xl' src="/public/cart-01.jpg" alt="cart" />
+      <img className='object-cover h-full object-center rounded-2xl' src="/public/cart-02.jpg" alt="cart" />
+      <img className='object-cover h-full object-center rounded-2xl' src="/public/cart-03.jpg" alt="cart" />
+      <img className='object-cover h-full object-center rounded-2xl' src="/public/details-1.jpg" alt="cart" />
+      <img className='object-cover h-full object-center rounded-2xl' src="/public/details-2.jpg" alt="cart" />
+      <img className='object-cover h-full object-center rounded-2xl' src="/public/details-3.jpg" alt="cart" />
+      
+    </div>
+  )
+}
+
+export default HomePage
