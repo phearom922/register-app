8bae266231f7e1389204011c2cc6e3823cc1c98e
phearom922 <ronphearom2540@gmail.com>
2025-02-10 15:59:12 +0700

    update images

--- src/component/HomePage.jsx
+++ src/component/HomePage.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-
+import ImageCart1 from "/public/cart-01.jpg"
+import ImageCart2 from "/public/cart-02.jpg"
 
 
 const HomePage = () => {
+  
   return (
     <div className='grid grid-cols-6 gap-5 py-10'>
-      <img className='object-cover h-full object-center rounded-2xl' src="./public/cart-01.jpg" alt="cart" />
-      <img className='object-cover h-full object-center rounded-2xl' src="./public/cart-02.jpg" alt="cart" />
-      <img className='object-cover h-full object-center rounded-2xl' src="./public/cart-03.jpg" alt="cart" />
-      <img className='object-cover h-full object-center rounded-2xl' src="./public/details-1.jpg" alt="cart" />
-      <img className='object-cover h-full object-center rounded-2xl' src="./public/details-2.jpg" alt="cart" />
-      <img className='object-cover h-full object-center rounded-2xl' src="./public/details-3.jpg" alt="cart" />
+      <img className='object-cover h-full object-center rounded-2xl' src={ImageCart1} alt="cart" />
+      <img className='object-cover h-full object-center rounded-2xl' src={ImageCart2} alt="cart" />
+    
       
     </div>
   )
