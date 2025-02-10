ecc9b8f7fd9cc4391ce545bbab8b8c40665e8fa7
phearom922 <ronphearom2540@gmail.com>
2025-02-10 15:28:14 +0700

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+{
+  basename: "/register-app"
+}
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
