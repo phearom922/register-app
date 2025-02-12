6180c9bb97df40cf9d6cae5789b05bbd7109f95c
phearom922 <ronphearom2540@gmail.com>
2025-02-12 11:34:58 +0700

    testing api

--- src/component/HomePage.jsx
+++ src/component/HomePage.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
-import ImageCart1 from "/public/cart-01.jpg"
-import ImageCart2 from "/public/cart-02.jpg"
-import ImageCart3 from "/public/cart-03.jpg"
-import ImageDetail1 from "/public/details-1.jpg"
-import ImageDetail2 from "/public/details-2.jpg"
-import ImageDetail3 from "/public/details-3.jpg"
-
+import React, { useState, useEffect } from "react";
+import ImageCart1 from "/public/cart-01.jpg";
+import ImageCart2 from "/public/cart-02.jpg";
+import ImageCart3 from "/public/cart-03.jpg";
+import ImageDetail1 from "/public/details-1.jpg";
+import ImageDetail2 from "/public/details-2.jpg";
+import ImageDetail3 from "/public/details-3.jpg";
 
 const HomePage = () => {
-  
-  return (
-    <div className='grid grid-cols-6 gap-5 py-10'>
-      <img className='object-cover h-full object-center rounded-2xl' src={ImageCart1} alt="cart" />
-      <img className='object-cover h-full object-center rounded-2xl' src={ImageCart2} alt="cart" />
-      <img className='object-cover h-full object-center rounded-2xl' src={ImageCart3} alt="cart" />
-      <img className='object-cover h-full object-center rounded-2xl' src={ImageDetail1} alt="cart" />
-      <img className='object-cover h-full object-center rounded-2xl' src={ImageDetail2} alt="cart" />
-      <img className='object-cover h-full object-center rounded-2xl' src={ImageDetail3} alt="cart" />
-    </div>
-  )
-}
+  const [products, setProducts] = useState([]);
 
-export default HomePage
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <>
+      <div className="grid grid-cols-5 gap-4">
+        {products.map((product) => (
+          <div
+            className="flex flex-col justify-between shadow w-full bg-gray-50 rounded-md"
+            key={product.id}
+          >
+            <div>
+              <img
+                className="w-full h-40 object-cover object-center "
+                src={product.image}
+                alt="photo"
+              />
+            </div>
+            <p className="pb-2 text-orange-800">{product.title}</p>
+            <p>{product.price}$</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
